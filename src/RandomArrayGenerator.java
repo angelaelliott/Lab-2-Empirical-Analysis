f8d853d95d3229d3d343be198a59471f9ccdb74f
angelaelliott <angelacelliott486@gmail.com>
2026-09-23 10:23:41 -0500

    Random Array Generator Class

---
+++ src/RandomArrayGenerator.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+
+public class RandomArrayGenerator {
+
+    public static int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] array = new int[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt();
+        }
+        return array;
+    }
+}
